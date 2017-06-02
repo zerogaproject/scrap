23ad0c9e58c3371087b7efb055c67c382f549e40
yo <yo@yo.com>
2017-06-01 21:52:51 -0300

    casisincro

--- extractor2.js
+++ extractor2.js
@@ -4,6 +4,18 @@ const mongojs = require('mongojs');
 const config = require('config');
 const db = mongojs(`${config.get('db').host}/${config.get('db').db}`,config.get('db').collections);
 
+const save = (item,categoryId) =>{
+  // items = items.map(item=> Object.assign({},item, {categoryId:categoryId}));
+  // db.products.insert(items,(err,docs)=>{
+  // })
+  db.products.findOne({olxid:item.id},{},(err,doc)=>{
+    if (!doc)
+    {
+       db.products.insert(Object.assign({},item, {categoryId:categoryId}));
+    }
+  })
+}
+
 const scrap = (url , categoryId) => {
 const mybrowser = new browser("mytest",{ttl:60});
 return  mybrowser.ready()
@@ -22,9 +34,10 @@ return  mybrowser.ready()
             var forReturn = [];
             for (var x = 0; x< items.length; x++){
               forReturn.push({
-                img: items[x].querySelectorAll("img").src,
+                img: items[x].querySelectorAll("img").src || "" ,
                 title: items[x].querySelectorAll("h3").innerText,
                 price:items[x].querySelectorAll(".items-price").innerText,
+                olxid:items[x].querySelectorAll("a").href.split("iid-")[1]
               });
             }
             return forReturn;
@@ -32,32 +45,39 @@ return  mybrowser.ready()
       })
       .then((items)=>{
         console.log(url, items);
-        items = items.map(item=> Object.assign({},item, {categoryId:categoryId}));
-        db.products.insert(items,(err,docs)=>{
+        if (items)
+        {
+            items.forEach(item=>{
+              save(item, categoryId);
+            });
+        }
+
           return Promise.resolve();
-        })
+
       })
       .then(()=>{
-          console.log("closing...");
-         return mybrowser.close();
+        //   console.log("closing...");
+        //  return mybrowser.close();
+        scrap(docs);
       })
       .catch((err)=>{
-          throw err;
+          //throw err;
+        scrap(docs);
       })
 
 
 };
-db.categories.find({},{},(err,docs)=>{
-  let tasks=[];
-  docs.forEach(category=>{
-    tasks.push(new Promise((resolve, reject)=>{
-      return scrap(category.href, category._id);
-    }))
-  });
-  Promise.all(tasks)
-  .then(()=>{
-    console.log('All end');
-    db.close();
-  })
-
+db.categories.find({}, {}, (err,docs)=>{
+  // let tasks=[];
+  // docs.forEach(category=>{
+  //   tasks.push(new Promise((resolve, reject)=>{
+  //     return scrap(category.href, category._id);
+  //   }))
+  // });
+  // Promise.all(tasks)
+  // .then(()=>{
+  //   console.log('All end');
+  //   db.close();
+  // })
+scrap(docs);
 });
